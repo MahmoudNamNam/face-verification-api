ea37c64a96db900a00c7a833e0fe5cb97cade1d1
MahmoudNamNam <mahmoud.namnam72@gmail.com>
2025-05-31 15:53:16 +0300

    update uvicorn configuration and refine requirements; remove vercel.json

--- main.py
+++ main.py
@@ -95,4 +95,4 @@ async def verify_face(request: FaceVerificationRequest):
 
 # Run the FastAPI app with Uvicorn when executed directly
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
